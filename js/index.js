8db58bc2a7035b802d09edd62d5d47b83b31505e
bliid <gh.bliid@gmail.com>
2020-08-09 14:14:36 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1200: {
+            items: 3
+        }
+    }
+})
+$(".test h1").text("title")
+console.log("test")
